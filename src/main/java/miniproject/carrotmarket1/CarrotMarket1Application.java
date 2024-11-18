c7c39152fd49f380dc492107394c74087de23bb7
rana0122 <rana0122@naver.com>
2024-11-18 18:49:41 +0900

    init

---
+++ src/main/java/miniproject/carrotmarket1/CarrotMarket1Application.java
@@ -0,0 +1,13 @@
+package miniproject.carrotmarket1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarket1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMarket1Application.class, args);
+    }
+
+}
