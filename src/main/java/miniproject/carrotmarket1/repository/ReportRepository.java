1c00e49a2746e981008a6105a173679422ac2019
kyusang0330 <cholongs123@naver.com>
2024-11-29 15:42:44 +0900

    report list 페이지네이션 - 항목별 검색 기능 추가

--- src/main/java/miniproject/carrotmarket1/repository/ReportRepository.java
+++ src/main/java/miniproject/carrotmarket1/repository/ReportRepository.java
@@ -1,7 +1,6 @@
 package miniproject.carrotmarket1.repository;
 
 import miniproject.carrotmarket1.entity.Report;
-import miniproject.carrotmarket1.entity.ReportStatus;
 
 import java.util.List;
 
@@ -9,14 +8,11 @@ import java.util.List;
 public interface ReportRepository {
 
     //신고 목록 조회(페이징 기능)
-    List<Report> getReportListPagination(String startDate, String endDate, String status, int size, int offset);
+    List<Report> getReportListPagination(String startDate, String endDate, String status, int size, int offset, String tag, String search);
     //신고 목록 조회(필터 기능)
     long countFilterReports(String startDate, String endDate, String status);
     //신고 상세 조회
     Report getReportById(Long id);
     //신고 처리 상태 변경
     void updateReportStatus(Report report);
-
-
-
 }
